009b8b6cace60a1a455af474c2824b2bdfc91380
rurico <106768848+ruaneenrico@users.noreply.github.com>
2022-10-27 17:36:33 -0300

    Add files via upload

---
+++ atividade aula 003/script.js
@@ -0,0 +1,33 @@
+/*Aries - de 21 março a 20 abril
+Touro -  de 21 abril a 20 maio
+Gemeos - de 21 maio a 20 junho
+Cancer - de 21 junho a 22 julho
+Leao - de 23 julho a 22 agosto
+Virgem - de 23 agosto a 22 setembro
+Caso não tenha o signo - Signo não registrado*/
+
+("-----------------------------------------------")
+
+var signo = prompt("digite seu signo");
+switch (signo){
+    case 'aries':
+        console.log('de 21 março a 20 abril');
+        break;
+        case 'touro':
+        console.log('de 21 abril a 20 maio');
+        break;
+        case 'gemeos':
+        console.log('de 21 maio a 20 junho');
+        break;
+        case 'cancer':
+        console.log('de 21 junho a 22 julho');
+        break;
+        case'leao':
+        console.log('de 23 julho a 22 agosto');
+        break;
+        case 'virgem':
+        console.log(' de 23 agosto a 22 setembro');
+
+
+
+}
